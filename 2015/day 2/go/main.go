a8e6771390f141c853158d3fb65c2899011cd76f
Anson Kan <48442757+ansonkan@users.noreply.github.com>
2023-10-31 22:47:58 +0800

    2015 day 2 part 2

--- 2015/day 2/go/main.go
+++ 2015/day 2/go/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	fmt.Println(Part1("../input.txt"))
+	fmt.Println(Part2("../input.txt"))
 }
 
 func check(e error) {
@@ -31,23 +31,43 @@ func Part1(filename string) int {
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
 		d := make([]int, 3)
-		var smallest int
 		for i, s := 0, strings.Split(scanner.Text(), "x"); i < len(s); i++ {
 			if num, err := strconv.Atoi(s[i]); err == nil {
 				d[i] = num
-
-				if i == 0 {
-					smallest = num
-				} else if num < smallest {
-					smallest = num
-				}
 			}
-
 		}
 
 		slices.Sort(d)
 
 		total += 2*(d[0]*d[1]+d[1]*d[2]+d[0]*d[2]) + d[0]*d[1]
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return total
+}
+
+func Part2(filename string) int {
+	f, open_err := os.Open(filename)
+	check(open_err)
+	defer f.Close()
+
+	total := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		d := make([]int, 3)
+		for i, s := 0, strings.Split(scanner.Text(), "x"); i < len(s); i++ {
+			if num, err := strconv.Atoi(s[i]); err == nil {
+				d[i] = num
+			}
+		}
+
+		slices.Sort(d)
+
+		total += 2*(d[0]+d[1]) + d[0]*d[1]*d[2]
 	}
 
 	if err := scanner.Err(); err != nil {
